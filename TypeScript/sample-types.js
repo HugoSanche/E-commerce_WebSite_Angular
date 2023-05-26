21b13911134da027fa76af01310fee942389382b
HugoSanche <hbmclaren@gmail.com>
2023-05-25 19:20:02 -0600

    My First TypeScript

---
+++ TypeScript/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.50;
+var firstName = "Hugo"; //double quaton
+var lastName = 'Baltazar'; //single quaton
+console.log(found);
+console.log("My grade " + grade);
+console.log("Mi nombre " + firstName);
+console.log("Mi apellido " + lastName);
---
+++ TypeScript/sample-types.ts
@@ -0,0 +1,11 @@
+let found: boolean=true;
+let grade: number=88.50;
+let firstName: string="Hugo"; //double quaton
+let lastName:string='Baltazar'; //single quaton
+
+console.log(found);
+console.log("My grade "+grade);
+console.log("Mi nombre "+firstName);
+console.log("Mi apellido "+lastName);
+
+
