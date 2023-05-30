8476e4320290bead71ae86f96a159b87b308e8cb
HugoSanche <hbmclaren@gmail.com>
2023-05-29 19:40:04 -0600

    Start the proyect

---
+++ ecommerce-proyect/02-backend/spring-boot-ecommerce/src/main/java/com/myproyects/ecommerce/entity/Product.java
@@ -0,0 +1,25 @@
+package com.myproyects.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+    private long id;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdate;
+
+}
---
+++ ecommerce-proyect/02-backend/spring-boot-ecommerce/src/test/java/com/myproyects/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproyects.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
