0009cb1d952f298fd2338617c8aa647ad32b2b16
HugoSanche <hbmclaren@gmail.com>
2023-05-30 23:08:26 -0600

    End develop JPA entities

--- ecommerce-proyect/02-backend/spring-boot-ecommerce/src/main/java/com/myproyects/ecommerce/entity/Product.java
+++ ecommerce-proyect/02-backend/spring-boot-ecommerce/src/main/java/com/myproyects/ecommerce/entity/Product.java
@@ -1,8 +1,9 @@
 package com.myproyects.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,15 +12,32 @@ import java.util.Date;
 @Table(name = "product")
 @Data
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+    @ManyToOne
+    @JoinColumn(name="category_id",nullable = false)
+    private ProductCategory category;
+    @Column(name = "sku")
     private String sku;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "unit_price")
     private BigDecimal unitPrice;
+    @Column(name = "image_url")
     private String imageUrl;
+    @Column(name = "active")
     private boolean active;
+    @Column(name = "units_in_stock")
     private int unitsInStock;
+    @Column(name = "date_created")
+    @CreationTimestamp
     private Date dateCreated;
+    @Column(name = "last_updated")
+    @UpdateTimestamp
     private Date lastUpdate;
 
 }
