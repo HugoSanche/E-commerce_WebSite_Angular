9266e230c405f7d17310757a0ec621855b65f24f
HugoSanche <hbmclaren@gmail.com>
2023-06-01 09:03:14 -0600

    Elimino mi carpeta

--- ecommerce-proyect/02-backend/spring-boot-ecommerce/src/main/java/com/myproyects/ecommerce/dao/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myproyects.ecommerce.dao;
-
-import com.myproyects.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Product= class product
-//Long= type date for the key of the class Product.- private Long id
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
