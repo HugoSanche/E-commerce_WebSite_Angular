a8e1989bc77da71a33eba716ad69a2e96419a32f
HugoSanche <hbmclaren@gmail.com>
2023-06-10 20:56:06 -0600

    eCommerce Project -Searh for Product

--- ecommerce-proyect/03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ ecommerce-proyect/03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,9 +9,16 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
   private baseUrl='http://localhost:8080/api/products';
+  
   constructor(private httpClient: HttpClient) { }
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+
+  getProductList(theCategoryId:number): Observable<Product[]>{
+
+    //need to build URL based on category id !
+    const searchUrl=`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    //se le asigna la variable que tiene la busqueda en este caso "searchUrl"
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response=>response._embedded.products)
     );
   }
