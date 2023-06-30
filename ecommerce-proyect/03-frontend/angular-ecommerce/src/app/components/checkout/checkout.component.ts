fbaa4dec00c51967bb89a129b9d6f613b2f6c681
HugoSanche <hbmclaren@gmail.com>
2023-06-30 17:34:36 -0600

    second part Checkout Form - Drop-Down Lists

--- ecommerce-proyect/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-proyect/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -84,4 +84,26 @@ export class CheckoutComponent implements OnInit{
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
   }
+  handleMonthsAndYears(){
+    const creditCardFormGroup=this.checkoutFormGroup.get('creditCard');
+    
+    const currentYear: number=new Date().getFullYear();
+    const selectedYear:number =Number(creditCardFormGroup.value.expirationYear);
+
+    //if the current year equals the selected year, then start with the current month
+    let startMonth:number;
+    if (currentYear==selectedYear){
+      startMonth= new Date().getMonth()+1;
+    }
+    else{
+      startMonth=1;
+    }
+    this.myShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data=>{
+      console.log("Retrieved credit card months: "+JSON.stringify(data));
+      this.creditCardMonths=data;
+      }
+    );
+  }
+
 }
