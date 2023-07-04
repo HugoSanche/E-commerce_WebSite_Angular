a415a632ebbbba634860719a6fb3958313ed5716
HugoSanche <hbmclaren@gmail.com>
2023-07-03 18:45:43 -0600

    First part Checkout Form - Display Validation Error

--- ecommerce-proyect/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce-proyect/03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { MyShopFormService } from 'src/app/services/my-shop-form.service';
@@ -29,9 +29,10 @@ export class CheckoutComponent implements OnInit{
   ngOnInit(): void {
     this.checkoutFormGroup=this.formBuilder.group({
       customer:this.formBuilder.group({
-        firstName:[''],
-        lastName:[''],
-        email:['']
+        firstName:new FormControl('',[Validators.required,Validators.minLength(2)]),
+        lastName:new FormControl('',[Validators.required,Validators.minLength(2)]),
+        email:new FormControl('',
+                            [Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress:this.formBuilder.group({
         country:[''],
@@ -88,6 +89,13 @@ export class CheckoutComponent implements OnInit{
   }
   onSubmitX(){
     console.log("Handling the submit button");
+
+    if (this.checkoutFormGroup.invalid){
+      //markAllAsTouched(): Touching all fields fields triggers the display of the error messanges
+
+        this.checkoutFormGroup.markAllAsTouched();
+
+    }
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The last Name of client "+this.checkoutFormGroup.get('customer').value.lastName);   
   
@@ -95,6 +103,12 @@ export class CheckoutComponent implements OnInit{
     console.log("The shipping Address state is "+this.checkoutFormGroup.get('shippingAddress').value.state.name);
   
   }
+
+get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
+get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
+get email(){return this.checkoutFormGroup.get('customer.email');}
+
+
   copyShippingAddressToBillingAddress(event){
     if (event.target.checked){
       this.checkoutFormGroup.controls['billingAddress']
